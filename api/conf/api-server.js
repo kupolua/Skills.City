fb495933446022ff6a52d2d6eb5323e9ab418b55
Pavel Kulakovsky <pkulakovsky@gmail.com>
2017-04-24 15:24:52 +0300

    fixed bug: move ENV to right place.

--- api/conf/api-server.js
+++ api/conf/api-server.js
@@ -10,7 +10,12 @@ app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 
 app.get('/api', function(req, res){
-    var API = require('../js/api.js');
+    var API = require('../js/api.js'),
+        db_host = process.env.DB_HOST,
+        db_login = process.env.DB_LOGIN,
+        db_password = process.env.DB_PASSWORD;
+
+    console.log(db_host, db_login, db_password);
 
     var api = new API();
 
